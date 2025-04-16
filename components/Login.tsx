a70f0503b6dd56a5a86293ecf3a111e614b42665
nimashakaranahala <nimasha.124@gmail.com>
2025-04-16 15:45:08 +0100

    add routes

--- components/Login.tsx
+++ components/Login.tsx
@@ -8,7 +8,8 @@ const Login: React.FC<LoginProps> = () => {
       <h2>Login</h2>
       {/* Add your login form or content here */}
       <button onClick={() => console.log('Login Submit')}>Login</button>
-      <p>Don't have an account? <a href="/createaccount">Create Account</a></p>
+      
+      <p>Don't have an account?  <a href="/createaccount">Create Account</a></p>
     </div>
   );
 };
